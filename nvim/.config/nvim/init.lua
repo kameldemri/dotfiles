c85cc662d2c870c8df1fd706d19f3b4a2eae5432
Kamel Demri <kameldemri00@gmail.com>
2025-07-28 15:24:33 +0100

    transparency settings

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -2,3 +2,7 @@ require("kamel.core")
 require("kamel.lazy")
 -- set up theme
 vim.cmd("colorscheme gruvbox")
+
+vim.cmd [[highlight Normal guibg=NONE ctermbg=NONE]]
+vim.cmd [[highlight NormalNC guibg=NONE ctermbg=NONE]]
+vim.cmd [[highlight Pmenu guibg=NONE]]
