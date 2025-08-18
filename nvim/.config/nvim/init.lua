1556ccb3e6987994e988b602d2d25f10c1812d5a
Kamel Demri <kameldemri00@gmail.com>
2025-08-18 16:23:58 +0100

    minor changes

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,8 +1,8 @@
 require("kamel.core")
 require("kamel.lazy")
 -- set up theme
-vim.cmd("colorscheme gruvbox")
+vim.cmd("colorscheme catppuccin-mocha")
 
-vim.cmd [[highlight Normal guibg=NONE ctermbg=NONE]]
-vim.cmd [[highlight NormalNC guibg=NONE ctermbg=NONE]]
-vim.cmd [[highlight Pmenu guibg=NONE]]
+vim.cmd([[highlight Normal guibg=NONE ctermbg=NONE]])
+vim.cmd([[highlight NormalNC guibg=NONE ctermbg=NONE]])
+vim.cmd([[highlight Pmenu guibg=NONE]])
